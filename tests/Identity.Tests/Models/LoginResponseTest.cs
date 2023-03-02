8e9723b3511ec95d66086ece4e0d059719ccbf23
viniciobs <vinicio.barreto.santos@gmail.com>
2023-03-01 21:59:20 -0300

    tests: Identity/Models

---
+++ tests/Identity.Tests/Models/LoginResponseTest.cs
@@ -0,0 +1,22 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using Identity.Models;
+
+namespace Identity.Tests.Models
+{
+	internal class LoginResponseTest
+	{
+		[Test]
+		public void GivenLoginResponse_WhenSuccess_ThenErrorMustBeEmptyAndNotNull()
+		{
+			// Arrange
+			var fixture = new Fixture();
+			var response = fixture.Create<LoginResponse>();
+
+			// Act
+
+			// Assert
+			response.Errors.Should().NotBeNull().And.BeEmpty();
+		}
+	}
+}
